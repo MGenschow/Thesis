20c9bf9d77ebf4e3a8005e622d45865a60818985
MGenschow <malte.genschow97@gmail.com>
2024-04-18 17:35:39 +0200

    restructured files

---
+++ 1_Generation/StyleGAN2-Ada/train_stylegan2_ada.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu_4
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:2
+#SBATCH --mem=14000
+#SBATCH --time=12:00:00
+#SBATCH --mail-user=malte.genschow@student.uni-tuebingen.de
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load devel/cuda/11.0
+export CXX=g++
+source /pfs/work7/workspace/scratch/tu_zxmav84-thesis/miniconda3/etc/profile.d/conda.sh
+conda activate thesis
+#echo "setup complete"
+python /pfs/work7/workspace/scratch/tu_zxmav84-thesis/Thesis/stylegan2-ada-pytorch/train.py \
+    --snap 10 \
+    --resume ffhq512 \
+    --outdir /pfs/work7/workspace/scratch/tu_zxmav84-thesis/Data.nosync/Models/Stylegan2_Ada/Experiments \
+    --data /pfs/work7/workspace/scratch/tu_zxmav84-thesis/Data.nosync/Zalando_Germany_Dataset/dresses/images/stylegan2_ada_images \
+    --kimg=1000 \
+    --gpus=2 \
+    --cfg=auto \
+    --mirror=1 \
+#    --dry-run
---
+++ 2_Inversion/e4e/e4e_training.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu_4
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=6000
+#SBATCH --time=08:00:00
+#SBATCH --mail-user=malte.genschow@student.uni-tuebingen.de
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load devel/cuda/11.0
+export CXX=g++
+source /pfs/work7/workspace/scratch/tu_zxmav84-thesis/miniconda3/etc/profile.d/conda.sh
+conda activate thesis
+#echo "setup complete"
+
+cd /pfs/work7/workspace/scratch/tu_zxmav84-thesis/Thesis/encoder4editing/
+
+python scripts/train.py \
+    --exp_dir=/pfs/work7/workspace/scratch/tu_zxmav84-thesis/Data.nosync/Models/e4e/experiments_default_lr/ \
+    --sub_exp_dir=setup \
+    --dataset_type zalando_germany_encode \
+    --workers 8 \
+    --batch_size 8 \
+    --test_batch_size 4 \
+    --test_workers 4 \
+    --stylegan_weights /pfs/work7/workspace/scratch/tu_zxmav84-thesis/Data.nosync/Models/e4e/pretrained/stylegan2_ada_zalando_germany.pt \
+    --stylegan_size 512 \
+    --learning_rate=0.0001 \
+    --start_from_latent_avg \
+    --use_w_pool \
+    --w_discriminator_lambda 0.1 \
+    --progressive_start 20000 \
+    --id_lambda 0.5 \
+    --max_steps 200000 \
+    --save_interval 2000 \
+    --val_interval 2000 \
+    --image_interval 500 \
+    --save_training_data
