6f30d977553e394ff5989156bf9ddcc88b6c0575
MGenschow <malte.genschow97@gmail.com>
2024-05-15 16:51:56 +0200

    added preparations for subsample PTI training

---
+++ 2_Inversion/PTI/pti_training.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu_4
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16000
+#SBATCH --time=00:20:00
+#SBATCH --mail-user=malte.genschow@student.uni-tuebingen.de
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load devel/cuda/11.0
+export CXX=g++
+source /pfs/work7/workspace/scratch/tu_zxmav84-thesis/miniconda3/etc/profile.d/conda.sh
+conda activate thesis
+echo "setup complete"
+
+cd /pfs/work7/workspace/scratch/tu_zxmav84-thesis/Thesis/PTI/
+
+python -c "from scripts.run_pti import run_PTI; run_PTI(run_name='whole_sample', use_wandb=False, use_multi_id_training=False)"
