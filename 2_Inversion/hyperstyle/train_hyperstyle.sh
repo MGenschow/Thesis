b1f8c9301d67a0bef93ee12f51b5293018079ec5
MGenschow <malte.genschow97@gmail.com>
2024-05-28 15:45:17 +0200

    resume hyperstyle training and add evaluation code

--- 2_Inversion/hyperstyle/train_hyperstyle.sh
+++ 2_Inversion/hyperstyle/train_hyperstyle.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
-#SBATCH --mem=40000
-#SBATCH --time=40:00:00
+#SBATCH --mem=16000
+#SBATCH --time=30:00:00
 #SBATCH --mail-user=malte.genschow@student.uni-tuebingen.de
 #SBATCH --mail-type=BEGIN,END,FAIL
 
@@ -20,7 +20,7 @@ cd /pfs/work7/workspace/scratch/tu_zxmav84-thesis/Thesis/hyperstyle/
 python scripts/train.py \
     --dataset_type=zalando_germany_encode \
     --encoder_type=SharedWeightsHyperNetResNet \
-    --exp_dir=/pfs/work7/workspace/scratch/tu_zxmav84-thesis/Data.nosync/Models/hyperstyle/00005_snapshot_1200_restyle_77000/ \
+    --exp_dir=/pfs/work7/workspace/scratch/tu_zxmav84-thesis/Data.nosync/Models/hyperstyle/00005_snapshot_1200_restyle_77000/resume/ \
     --stylegan_weights /pfs/work7/workspace/scratch/tu_zxmav84-thesis/Data.nosync/Models/hyperstyle/pretrained/00005-stylegan2_ada_images-mirror-auto2-kimg5000-resumeffhq512_network-snapshot-001200.pt \
     --workers=8 \
     --batch_size=8 \
@@ -40,3 +40,4 @@ python scripts/train.py \
     --input_nc=6 \
     --load_w_encoder \
     --w_encoder_checkpoint_path=/pfs/work7/workspace/scratch/tu_zxmav84-thesis/Data.nosync/Models/restyle/00005_snapshot_1200/checkpoints/best_model.pt \
+    --checkpoint_path=/pfs/work7/workspace/scratch/tu_zxmav84-thesis/Data.nosync/Models/hyperstyle/00005_snapshot_1200_restyle_77000/checkpoints/iteration_40000.pt
